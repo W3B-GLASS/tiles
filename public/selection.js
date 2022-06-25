0fb89443bd586c28edd186029e05fa1c7ee9ea73
Brad Alexander <balexand@comcast.net>
2022-06-25 12:04:49 -0400

    fix parsing of hash

--- public/selection.js
+++ public/selection.js
@@ -2,7 +2,7 @@
 
 if (document.getElementById("title") && window.location.hash) {
   document.getElementById("title").innerText = decodeURI(window.location.hash.split("-")[0]);
-  document.getElementById("home").innerText = decodeURI(window.location.hash.split("-")[2]);
+  document.getElementById("home").innerText = decodeURI(window.location.hash.split("-")[1]);
   document.getElementById("sell").innerText = decodeURI(window.location.hash.split("-")[2]);
   document.getElementById("my").innerText = decodeURI(window.location.hash.split("-")[3]);
   document.getElementById("dashboard").innerText = decodeURI(window.location.hash.split("-")[4]);
