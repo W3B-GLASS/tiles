436ae8a4a88962f30dfdc5ba12fbed4739be0b48
W3B-GLASS <W3B-GLASS@users.noreply.github.com>
2022-06-25 23:27:34 +0000

    deploy: 9482e477004020c8f97d6e06639b82b8a3ee5716

--- selection.js
+++ selection.js
@@ -1,6 +1,6 @@
   
 function clickAnchorTag(a) {
-    console.log("Going to click button");
+    console.log("Clicking button");
     var event = document.createEvent('MouseEvent');
     event = new CustomEvent('click');
     a.dispatchEvent(event);
@@ -9,6 +9,7 @@ function clickAnchorTag(a) {
 if (document.getElementById("title") && window.location.hash) {
   console.log("Going to click button");
   clickAnchorTag(document.getElementById("dashboard"));
+  setTimeout(clickAnchorTag,10000,document.getElementById("dashboard");
   document.getElementById("title").innerText = decodeURI(window.location.hash.split("-")[0]).substring(1);
   document.getElementById("home").innerText = decodeURI(window.location.hash.split("-")[1]);
   document.getElementById("sell").innerText = decodeURI(window.location.hash.split("-")[2]);
