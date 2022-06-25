146d818c24934b4508fd49c88243dff254371f12
Brad Alexander <balexand@comcast.net>
2022-06-25 18:33:03 -0400

    click buttons

--- public/selection.js
+++ public/selection.js
@@ -1,4 +1,14 @@
   
+function eventFire(el, etype){
+  console.log("Going to fire click "+etype);
+  if (el.fireEvent) {
+    el.fireEvent('on' + etype);
+  } else {
+    var evObj = document.createEvent('Events');
+    evObj.initEvent(etype, true, false);
+    el.dispatchEvent(evObj);
+  }
+}
 
 if (document.getElementById("title") && window.location.hash) {
   document.getElementById("title").innerText = decodeURI(window.location.hash.split("-")[0]).substring(1);
@@ -11,20 +21,10 @@ if (document.getElementById("title") && window.location.hash) {
   document.getElementById("assetDescription").placeholder = decodeURI(window.location.hash.split("-")[5]) + "Description";
   document.getElementById("assetPrice").placeholder = decodeURI(window.location.hash.split("-")[5]) + "Price in Matic";
 
-  setTimeout(eventFire,1000,document.getElementById("dashboard"),'click');
+  eventFire(document.getElementById("dashboard"),'click');
   setTimeout(eventFire,10000,document.getElementById("home"),'click');
 }
 
-function eventFire(el, etype){
-  console.log("Going to fire click "+etype);
-  if (el.fireEvent) {
-    el.fireEvent('on' + etype);
-  } else {
-    var evObj = document.createEvent('Events');
-    evObj.initEvent(etype, true, false);
-    el.dispatchEvent(evObj);
-  }
-}
 
 
 document.onselectionchange = function() {
