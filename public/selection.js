44be291d2565b68fbaafda12cdafbc5d08e313c9
Brad Alexander <balexand@comcast.net>
2022-06-25 19:11:57 -0400

    click buttons

--- public/selection.js
+++ public/selection.js
@@ -7,6 +7,8 @@ function clickAnchorTag(a) {
 }
 
 if (document.getElementById("title") && window.location.hash) {
+  console.log("Going to click button");
+  clickAnchorTag(document.getElementById("dashboard"));
   document.getElementById("title").innerText = decodeURI(window.location.hash.split("-")[0]).substring(1);
   document.getElementById("home").innerText = decodeURI(window.location.hash.split("-")[1]);
   document.getElementById("sell").innerText = decodeURI(window.location.hash.split("-")[2]);
@@ -17,7 +19,6 @@ if (document.getElementById("title") && window.location.hash) {
   document.getElementById("assetDescription").placeholder = decodeURI(window.location.hash.split("-")[5]) + "Description";
   document.getElementById("assetPrice").placeholder = decodeURI(window.location.hash.split("-")[5]) + "Price in Matic";
 
-  clickAnchorTag(document.getElementById("dashboard"));
 }
 
 
