21d9df195b5622a6b84cad498d7b7f743bf56e0b
Brad Alexander <balexand@comcast.net>
2022-06-25 17:02:05 -0400

    debug resell

--- vendor/github.com/dabit3/polygon-ethereum-nextjs-marketplace/next.config.js
+++ vendor/github.com/dabit3/polygon-ethereum-nextjs-marketplace/next.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: '/marketplace'
-//  publicRuntimeConfig: { 
-//    staticFolder: `/marketplace/static`
-//  },
+  assetPrefix: '/marketplace',
+  publicRuntimeConfig: { 
+    staticFolder: `/marketplace/static`
+  },
 //  webpack: (config) => {
     // Fixes npm packages that depend on `fs` module
 //    config.node = {
