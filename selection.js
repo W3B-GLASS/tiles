af493fad44b998100c8cca117ffeb3021f6299f2
W3B-GLASS <W3B-GLASS@users.noreply.github.com>
2022-06-25 16:59:34 +0000

    deploy: 3625cf41a2e3d546390689707b791cc7b0a8e48b

--- selection.js
+++ selection.js
@@ -1,7 +1,7 @@
   
 
 if (document.getElementById("title") && window.location.hash) {
-  document.getElementById("title").innerText = decodeURI(window.location.hash.split("-")[0]);
+  document.getElementById("title").innerText = decodeURI(window.location.hash.split("-")[0]).substring(1);
   document.getElementById("home").innerText = decodeURI(window.location.hash.split("-")[1]);
   document.getElementById("sell").innerText = decodeURI(window.location.hash.split("-")[2]);
   document.getElementById("my").innerText = decodeURI(window.location.hash.split("-")[3]);
