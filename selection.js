fc4c6b5d68953ef7409f06c978904a1e2ae5a544
W3B-GLASS <W3B-GLASS@users.noreply.github.com>
2022-06-26 05:21:20 +0000

    deploy: ed09785d28cf91ff22951a760619d7bad0ed323a

--- selection.js
+++ selection.js
@@ -8,12 +8,6 @@ function clickAnchorTag(a) {
 
 if (document.getElementById("title") && window.location.hash) {
   console.log("Update header");
-  document.onselectionchange = function() {
-    console.log('New selection made');
-    let selection = document.getSelection();
-    console.log('selection is '+selection);
-    console.dir(selection);
-  };
   document.getElementById("title").innerText = decodeURI(window.location.hash.split("-")[0]).substring(1);
   document.getElementById("home").innerText = decodeURI(window.location.hash.split("-")[1]);
   document.getElementById("sell").innerText = decodeURI(window.location.hash.split("-")[2]);
@@ -31,7 +25,11 @@ if (document.getElementById("title") && window.location.hash) {
   document.getElementById("title").after(frame);
 }
 
-
+window.addEventListener("message", (event) => {
+  // this is the magic connection:
+  console.log("selection.js Got message " + event.source); 
+  console.dir(event.source); 
+}, false);
 
 document.onselectionchange = function() {
     console.log('New selection made');
