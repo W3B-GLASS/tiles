4c79b4d6299e47e76c4d1f25606408779cc235b7
Brad Alexander <balexand@comcast.net>
2022-06-26 01:39:52 -0400

    overlap windows

--- vendor/github.com/rlamana/Ventus/examples/desktop/init.js
+++ vendor/github.com/rlamana/Ventus/examples/desktop/init.js
@@ -2,8 +2,8 @@
 
 function doSomethingLater() {
 	const w3b_bio = "nft-dashboard-template/#Web3 Bio-Bios-Add A Bio-Your Bio Links-Dashboard-Bio-0xb745845A511fF4D328D534c59A6eBb547a57D9B0";
-//	const lens_bio = "lens-protocol-frontend/profiles"
-//        document.getElementById("lens_bio").src = lens_bio;
+	const lens_bio = "lens-protocol-frontend/profiles"
+        document.getElementById("lens_bio").src = lens_bio;
         document.getElementById("w3b_bio").src = w3b_bio;
 
 	const w3b_store = "nft-dashboard-template/#Web3 Stor3-Apps-Add An App-Your Apps-Dashboard-App-0x94F4a38726570f41693CD1aC9DefFab22AF819b4";
